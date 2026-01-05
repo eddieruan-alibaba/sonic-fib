4144661d0e45b5f19ae48d283b46f9709834a864
Yuqing Zhao <galadriel.zyq@alibaba-inc.com>
2026-01-05 13:53:08 +0800

    Add c-api UT for singleton case
    Signed-off-by: Yuqing Zhao <galadriel.zyq@alibaba-inc.com>

--- tests/c_api_ut.cpp
+++ tests/c_api_ut.cpp
@@ -119,3 +119,161 @@ TEST(NextHopGroupFull_CAPI, multi_nexthop) {
 
     cout << "TEST_NextHopGroupFull_CAPI::multi_nexthop finished." << endl;
 }
+
+TEST(NextHopGroupFull_CAPI, singleton) {
+    cout << "TEST_NextHopGroupFull_CAPI::singleton started:" << endl;
+    cout << "[DEBUG] Constructing C_NextHopGroupFull type singleton values ..." << endl;
+
+    /* Prepare the parameters */
+    C_NextHopGroupFull c_nhg = {};
+
+    c_nhg.id = 100;
+    c_nhg.key = 1234567;
+    c_nhg.type = NEXTHOP_TYPE_IPV6;
+    c_nhg.vrf_id = 101;
+    c_nhg.ifindex = 101;
+    c_nhg.nh_label_type = ZEBRA_LSP_NONE;
+    c_nhg.bh_type = BLACKHOLE_NULL;
+    c_nhg.weight = 8;
+    c_nhg.flags = 8;
+
+    // set up addresses
+    inet_pton(AF_INET6, "2001:db8::1", &c_nhg.gate.ipv6.s6_addr);
+    inet_pton(AF_INET6, "2001:db8::2", &c_nhg.src.ipv6.s6_addr);
+    inet_pton(AF_INET6, "2001:db8::3", &c_nhg.rmap_src.ipv6.s6_addr);
+
+    // set up depends and dependents
+    c_nhg.depends[0] = 200;
+    c_nhg.depends[1] = 300;
+    c_nhg.depends[2] = 400;
+    c_nhg.depends[3] = 0;
+
+    c_nhg.dependents[0] = 500;
+    c_nhg.dependents[1] = 600;
+    c_nhg.dependents[2] = 0;
+
+    // prepare segment list
+    vector<struct in6_addr> test_nh_segs(3);
+    inet_pton(AF_INET6, "2001:db8:1::1", test_nh_segs[0].s6_addr);
+    inet_pton(AF_INET6, "2001:db8:1::2", test_nh_segs[1].s6_addr);
+    inet_pton(AF_INET6, "2001:db8:1::3", test_nh_segs[2].s6_addr);
+
+    // prepare seg6_segs
+    size_t total = sizeof(struct seg6_seg_stack) +
+                            test_nh_segs.size() * sizeof(struct in6_addr);
+    struct seg6_seg_stack* test_nh_seg6_segs =
+        (struct seg6_seg_stack*)malloc(total);
+    test_nh_seg6_segs->encap_behavior = SRV6_HEADEND_BEHAVIOR_H_ENCAPS;
+    test_nh_seg6_segs->num_segs = 3;
+    memcpy(test_nh_seg6_segs->seg, test_nh_segs.data(),
+           test_nh_segs.size() * sizeof(in6_addr));
+
+    // Prepare seg6local_flavors_info
+    struct seg6local_flavors_info test_flv = {
+        .flv_ops = 100,
+        .lcblock_len = 20,
+        .lcnode_func_len = 16
+    };
+
+    // Prepare seg6local_ctx
+    struct seg6local_context test_seg6local_ctx = {};
+    inet_pton(AF_INET, "192.168.10.1", &test_seg6local_ctx.nh4.s_addr);
+    inet_pton(AF_INET6, "2001:db8::a", &test_seg6local_ctx.nh6.s6_addr);
+    test_seg6local_ctx.table = 100;
+    test_seg6local_ctx.block_len = 36;
+    test_seg6local_ctx.node_len = 12;
+    test_seg6local_ctx.function_len = 20;
+    test_seg6local_ctx.argument_len = 16;
+    memcpy(&test_seg6local_ctx.flv, &test_flv, sizeof(struct seg6local_flavors_info));
+
+    // Prepare nh_srv6
+    struct nexthop_srv6* test_nh_srv6 =
+        (struct nexthop_srv6*)malloc(sizeof(struct nexthop_srv6));
+    memset(test_nh_srv6, 0, sizeof(struct nexthop_srv6));
+    test_nh_srv6->seg6local_action = SEG6_LOCAL_ACTION_END_DT6;
+    memcpy(&test_nh_srv6->seg6local_ctx, &test_seg6local_ctx,
+           sizeof(struct seg6local_context));
+    test_nh_srv6->seg6_segs = test_nh_seg6_segs;
+
+    c_nhg.nh_srv6 = test_nh_srv6;
+
+    /* Call c-api to convert C_NextHopGroupFull to C++ NextHopGroupFull and return JSON string */
+    cout << "[DEBUG] Calling nexthopgroupfull_json_from_c_nhg_singleton ..." << endl;
+    char* json_str = nexthopgroupfull_json_from_c_nhg_singleton(&c_nhg, MULTIPATH_NUM);
+
+    ASSERT_NE(json_str, nullptr) << "[ERROR] C-API returned nullptr";
+
+    /* Output the generated JSON string */
+    cout << "    [DEBUG] The generated JSON string is:" << endl;
+    cout << "    " << json_str << endl;
+
+    /* Parse JSON string and deserialize to C++ object */
+    cout << "[DEBUG] Parsing JSON string and deserializing to C++ object ..." << endl;
+    nlohmann::ordered_json j = nlohmann::json::parse(json_str);
+    fib::NextHopGroupFull cpp_nhg;
+    from_json(j, cpp_nhg);
+
+    /* Verify round-trip conversion by comparing C structure and C++ object */
+    cout << "[DEBUG] Verifying C_NextHopGroupFull vs C++ NextHopGroupFull ..." << endl;
+    // verify basic fields
+    EXPECT_EQ(cpp_nhg.id, c_nhg.id);
+    EXPECT_EQ(cpp_nhg.key, c_nhg.key);
+    EXPECT_EQ(cpp_nhg.type, c_nhg.type);
+    EXPECT_EQ(cpp_nhg.vrf_id, c_nhg.vrf_id);
+    EXPECT_EQ(cpp_nhg.ifindex, c_nhg.ifindex);
+    EXPECT_EQ(cpp_nhg.weight, c_nhg.weight);
+    EXPECT_EQ(cpp_nhg.flags, c_nhg.flags);
+    EXPECT_EQ(cpp_nhg.nh_label_type, c_nhg.nh_label_type);
+    EXPECT_EQ(cpp_nhg.bh_type, c_nhg.bh_type);
+
+    // verify addresses
+    EXPECT_EQ(memcmp(&cpp_nhg.gate.ipv6, &c_nhg.gate.ipv6, sizeof(struct in6_addr)), 0);
+    EXPECT_EQ(memcmp(&cpp_nhg.src.ipv6, &c_nhg.src.ipv6, sizeof(struct in6_addr)), 0);
+    EXPECT_EQ(memcmp(&cpp_nhg.rmap_src.ipv6, &c_nhg.rmap_src.ipv6, sizeof(struct in6_addr)), 0);
+
+    // verify depends
+    EXPECT_EQ(cpp_nhg.depends.size(), 3);
+    EXPECT_EQ(cpp_nhg.depends[0], c_nhg.depends[0]);
+    EXPECT_EQ(cpp_nhg.depends[1], c_nhg.depends[1]);
+    EXPECT_EQ(cpp_nhg.depends[2], c_nhg.depends[2]);
+
+    // verify dependents
+    EXPECT_EQ(cpp_nhg.dependents.size(), 2);
+    EXPECT_EQ(cpp_nhg.dependents[0], c_nhg.dependents[0]);
+    EXPECT_EQ(cpp_nhg.dependents[1], c_nhg.dependents[1]);
+
+    // verify SRv6 basic information
+    ASSERT_NE(cpp_nhg.nh_srv6, nullptr) << "nh_srv6 should not be null";
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_action, c_nhg.nh_srv6->seg6local_action);
+    // verify seg6local_ctx
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.table, c_nhg.nh_srv6->seg6local_ctx.table);
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.block_len, c_nhg.nh_srv6->seg6local_ctx.block_len);
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.node_len, c_nhg.nh_srv6->seg6local_ctx.node_len);
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.function_len, c_nhg.nh_srv6->seg6local_ctx.function_len);
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.argument_len, c_nhg.nh_srv6->seg6local_ctx.argument_len);
+    // verify seg6local_ctx.flv
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.flv.flv_ops, c_nhg.nh_srv6->seg6local_ctx.flv.flv_ops);
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.flv.lcblock_len, c_nhg.nh_srv6->seg6local_ctx.flv.lcblock_len);
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6local_ctx.flv.lcnode_func_len, c_nhg.nh_srv6->seg6local_ctx.flv.lcnode_func_len);
+    // verify seg6local_ctx nh4 and nh6
+    EXPECT_EQ(memcmp(&cpp_nhg.nh_srv6->seg6local_ctx.nh4, &c_nhg.nh_srv6->seg6local_ctx.nh4,
+                     sizeof(struct in_addr)), 0);
+    EXPECT_EQ(memcmp(&cpp_nhg.nh_srv6->seg6local_ctx.nh6, &c_nhg.nh_srv6->seg6local_ctx.nh6,
+                     sizeof(struct in6_addr)), 0);
+    // verify seg6_segs
+    ASSERT_NE(cpp_nhg.nh_srv6->seg6_segs, nullptr) << "seg6_segs should not be null";
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6_segs->encap_behavior, c_nhg.nh_srv6->seg6_segs->encap_behavior);
+    EXPECT_EQ(cpp_nhg.nh_srv6->seg6_segs->num_segs, c_nhg.nh_srv6->seg6_segs->num_segs);
+    // verify segment list
+    for (size_t i = 0; i < cpp_nhg.nh_srv6->seg6_segs->num_segs; i++) {
+        EXPECT_EQ(memcmp(&cpp_nhg.nh_srv6->seg6_segs->seg[i], &c_nhg.nh_srv6->seg6_segs->seg[i],
+                         sizeof(struct in6_addr)), 0);
+    }
+
+    /* Clean up */
+    free(json_str);
+    free(test_nh_srv6->seg6_segs);
+    free(test_nh_srv6);
+
+    cout << "TEST_NextHopGroupFull_CAPI::singleton finished." << endl;
+}
